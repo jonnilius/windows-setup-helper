e87398da138161465ea17d805e041b0bb502d545
John-Andreas <john.andreas@lokal>
2026-05-06 14:12:51 +0200

    Füge Funktionen zur dynamischen Schriftartverwaltung hinzu und verbessere die Schriftartvorgaben

--- Modules/FormBuilder/SubModules/Font.psm1
+++ Modules/FormBuilder/SubModules/Font.psm1
@@ -1,5 +1,66 @@
 using namespace System.Drawing
 using namespace System.Windows.Forms
+$FontKeys = @("Size", "Name", "Style")
+$DefaultFont = @{ Size = 10; Name = "Consolas"; Style = "Regular" }
+function Get-ControlFont {
+    param( [string]$Control )
+    
+    $font = @{
+            Button          = @{}
+            CheckBox        = @{ Size = 9; }
+            CheckedListBox  = @{ Size = 9; }
+            ComboBox        = @{}
+            GroupBox        = @{}
+            Label           = @{ Name = "Tahoma" }
+            ListBox         = @{}
+            ListView        = @{}
+            RichTextBox     = @{}
+            TabControl      = @{}
+            TabPage         = @{}
+            TextBox         = @{ Size = 12; Name = "Segoe UI";  Style = "Bold" }
+        }[$Control]
+    foreach ($key in $FontKeys) { if (-not $font.ContainsKey($key)) { $font[$key] = $DefaultFont[$key] } }
+    return $font
+}
+function Get-PresetFont {
+    param( [string]$Preset )
+    $font = @{
+            # Label-Presets
+            Header          = @{ Size = 25; Name = "Cascadia Code"; Style = "Bold" }
+            LabelButton     = @{ Size = 8 }
+            LabelItalic     = @{ Name = "Tahoma";    Style = "Italic" }
+
+            # Tab-Presets
+            TabLabel        = @{ Style = "Italic" }
+
+            # Table-Presets
+            TableTitle      = @{ Size = 14; Name = "Segoe UI";    Style = @("Bold", "Underline") }
+            TableLabel      = @{ Name = "Tahoma";    Style = "Bold" }
+            TableText       = @{ Name = "Tahoma" }
+            TableTextHover  = @{ Name = "Tahoma";    Style = "Italic" }
+            TableLink       = @{ Size = 9;  Name = "Tahoma";    Style = "Italic" }
+            TableButton     = @{ Size = 8;  Name = "Tahoma";    Style = "Bold" }
+            
+            # Sidebar Presets
+            SidebarTitle            = @{ Size = 22; Name = "Cascadia Code"; Style = "Bold" }
+            SidebarVersion          = @{}
+            SidebarButton           = @{ Size = 8;  Name = "Segoe UI";      Style = "Bold" }
+            PackageInfoTitle        = @{ Name = "Tahoma";        Style = "Bold" }
+            PackageInfoLabel        = @{ Size = 9;  Name = "Segoe UI";      Style = "Bold" }
+            PackageInfoDescription  = @{ Size = 9;  Name = "Segoe UI" }
+            # Form Presets
+            FooterLink      = @{ Size = 8;  Name = "Tahoma";    Style = "Bold" }
+            FooterLinkHover = @{ Size = 8;  Name = "Tahoma";    Style = @("Bold", "Underline") }
+            FooterText      = @{ Size = 9;  Style = "Italic" }
+
+            # Other Presets
+            SearchHeader    = @{ Size = 20; Name = "Cascadia Code"; Style = "Bold" }
+            Title           = @{ Size = 18; Name = "Segoe UI";      Style = "Bold" }
+            Subtitle        = @{ Size = 13; Name = "Segoe UI";      Style = @("Bold", "Underline") }
+        }[$Preset]
+    foreach ($key in $FontKeys) { if (-not $font.ContainsKey($key)) { $font[$key] = $DefaultFont[$key] } }
+    return $font
+}
 
 function Get-Font {
     param(
@@ -9,67 +70,13 @@ function Get-Font {
         [string[]]$Style,
         [string]$Preset
     )
-
     # Vordefinierte Schriftarteinstellungen basierend auf Control-Typ oder Preset auswählen
-    if ($Control) {
-        $fontPreset = @{
-            Button          = @{ Size = 10; Name = "Consolas";  Style = "Regular" }
-            CheckBox        = @{ Size = 9;  Name = "Consolas";  Style = "Regular" }
-            CheckedListBox  = @{ Size = 9;  Name = "Consolas";  Style = "Regular" }
-            ComboBox        = @{ Size = 10; Name = "Consolas";  Style = "Regular" }
-            GroupBox        = @{ Size = 10; Name = "Consolas";  Style = "Regular" }
-            Label           = @{ Size = 10; Name = "Tahoma";    Style = "Regular" }
-            ListBox         = @{ Size = 10; Name = "Consolas";  Style = "Regular" }
-            ListView        = @{ Size = 10; Name = "Consolas";  Style = "Regular" }
-            RichTextBox     = @{ Size = 10; Name = "Consolas";  Style = "Regular" }
-            TabControl      = @{ Size = 10; Name = "Consolas";  Style = "Regular" }
-            TabPage         = @{ Size = 10; Name = "Consolas";  Style = "Regular" }
-            TextBox         = @{ Size = 12; Name = "Segoe UI";  Style = "Bold" }
-        }[$Control]
-    } elseif ($Preset) {
-        $fontPreset = @{
-            # Hybrid Controls
-            LabelButton     = @{ Size = 8;  Name = "Consolas";      Style = "Regular" }
-            LabelItalic     = @{ Size = 10; Name = "Tahoma";        Style = "Italic" }
-            
-            # TabControl Presets
-            TabLabel        = @{ Size = 10; Name = "Consolas";  Style = "Italic" }
-
-            # Table Presets
-            TableTitle      = @{ Size = 14; Name = "Tahoma"; Style = "Bold" }
-            TableLabel      = @{ Size = 10; Name = "Tahoma"; Style = "Bold" }
-            TableText       = @{ Size = 10; Name = "Tahoma"; Style = "Regular" }
-            TableTextHover  = @{ Size = 10; Name = "Tahoma"; Style = "Italic" }
-            TableLink       = @{ Size = 9;  Name = "Tahoma"; Style = "Italic" }
-            TableButton     = @{ Size = 8;  Name = "Tahoma"; Style = "Bold" }
-
-            # Sidebar Presets
-            SidebarTitle            = @{ Size = 22; Name = "Cascadia Code"; Style = "Bold" }
-            SidebarVersion          = @{ Size = 10; Name = "Consolas";      Style = "Regular" }
-            SidebarButton           = @{ Size = 8;  Name = "Segoe UI";      Style = "Bold" }
-            PackageInfoTitle        = @{ Size = 10; Name = "Tahoma";        Style = "Bold" }
-            PackageInfoLabel        = @{ Size = 9;  Name = "Segoe UI";      Style = "Bold" }
-            PackageInfoDescription  = @{ Size = 9;  Name = "Segoe UI";      Style = "Regular" }
-            
-            # Form Presets
-            FooterLink      = @{ Size = 8;  Name = "Tahoma";    Style = "Bold" }
-            FooterLinkHover = @{ Size = 8;  Name = "Tahoma";    Style = @("Bold", "Underline") }
-            FooterText      = @{ Size = 9;  Name = "Consolas";  Style = "Italic" }
-
-            # Other Presets
-            SearchHeader    = @{ Size = 20; Name = "Cascadia Code"; Style = "Bold" }
-            Title           = @{ Size = 18; Name = "Segoe UI";      Style = "Bold" }
-            Subtitle        = @{ Size = 13; Name = "Segoe UI";      Style = @("Bold", "Underline") }
-        }[$Preset]
-    } else {
-        $fontPreset = $null
-    }
-    if (-not $fontPreset) { $fontPreset = @{ Size = 10; Name = "Consolas"; Style = "Regular" } }
+    $fontPreset = if ($Control) { Get-ControlFont -Control $Control } elseif ($Preset) { Get-PresetFont -Preset $Preset }
 
     # Bevorzugte Schriftarten definieren und die erste verfügbare auswählen
-    $Size  = If ($Size) { $Size } else { $fontPreset.Size }
-    $Name  = If ($Name) { $Name } else { $fontPreset.Name }
-    $Style = If ($Style) { $Style } else { $fontPreset.Style }
+    $Size  = If ($Size)  { $Size }  elseif ($fontPreset.Size) { $fontPreset.Size } else { $DefaultPreset.Size }
+    $Name  = If ($Name)  { $Name }  elseif ($fontPreset.Name) { $fontPreset.Name } else { $DefaultPreset.Name }
+    $Style = If ($Style) { $Style } elseif ($fontPreset.Style) { $fontPreset.Style } else { $DefaultPreset.Style }
 
 
     # FontStyle-Enum aus einem oder mehreren übergebenen Styles aufbauen
